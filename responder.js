86b66ea37abc0ecba43c39381ada76b2a0bffe67
Linus Miller <lohfu@lohfu.io>
2016-08-02 17:50:35 +0200

    Handle 'html' and '*/*' differently in responder.

--- responder.js
+++ responder.js
@@ -14,25 +14,47 @@ const _ = require('lodash');
 const debug = require('debug')('warepot:responder');
 
 module.exports = function responder(req, res) {
+  function sendJSON() {
+    const keys = Object.keys(res.locals);
+
+    /* if there is only a single property on `res.locals` and it is not a page
+     * object (this can be a model/object or collection/array) then only
+     * return that property.
+     */
+
+    res.json(keys.length === 1 && keys[0] !== 'page' ? res.locals[keys[0]] : _.omit(res.locals, 'query'));
+  }
+
   res.format({
     json() {
       debug('ACCEPTS json, returning json');
 
-      /* if there is only a single property on `res.locals` and it is not a page
-       * object (this can be a model/object or collection/array) then only
-       * return that property.
-       */
-      const keys = Object.keys(res.locals);
+      sendJSON();
+    },
 
-      res.json(keys.length === 1 && keys[0] !== 'page' ? res.locals[keys[0]] : _.omit(res.locals, 'query'));
+    html() {
+      debug('ACCEPTS html, returning html');
+
+      if (res.template || res.master)
+        return res.render(res.template || res.master);
+
+      res.send('<pre>' + JSON.stringify(res.locals, null, '  ') + '</pre>');
     },
 
     '*/*'() {
-      debug('ACCEPTS html, returning html');
+      debug('ACCEPTS */*...');
 
-      res.set('Content-Type', 'text/html');
+      if (res.template) {
+        debug('res.template set, sending HTML.');
 
-      res.render(res.template || res.master);
+        res.set('Content-Type', 'text/html');
+
+        return res.render(res.template || res.master);
+      }
+
+      debug('res.template not set, sending JSON.');
+
+      sendJSON();
     }
   });
 };
