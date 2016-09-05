18e1eb1e877a5700b3412fa4c4aeec3f3433d7a0
Linus Miller <lohfu@lohfu.io>
2016-09-05 16:03:47 +0200

    set-master > set-template

---
+++ middleware/set-template.js
@@ -0,0 +1,17 @@
+/*
+ * Simply sets res.template.
+ *
+ * @module midwest/set-template
+ */
+'use strict'
+
+/* Middleware factory
+ *
+ * @param {} template - Template to set on `res`.
+ *
+ * @returns A middleware function
+ */
+module.exports = (template) => function setTemplate(req, res, next) {
+  res.template = template
+  next()
+}
