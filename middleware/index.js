e02b779fc7f6de3f441cffd5c186f32879ecdfcb
Linus Miller <lohfu@lohfu.io>
2016-10-21 17:59:43 +0200

    Remove get-languages middleware

--- middleware/index.js
+++ middleware/index.js
@@ -5,7 +5,6 @@ module.exports = {
   ensureFound: require('./ensure-found.js'),
   errorHandler: require('./error-handler.js'),
   formatQuery: require('./format-query.js'),
-  getLanguages: require('./get-languages.js'),
   page: require('./page.js'),
   paginate: require('./paginate.js'),
   parallel: require('./parallel.js'),
