e85a6c9daea050e4d9aaaf38522b87cebb9f81ac
Linus Miller <lohfu@lohfu.io>
2016-10-31 15:26:29 +0100

    Split middleware dir into factories and middleware

---
+++ factories/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  bootstrap: require('./bootstrap.js'),
+  errorHandler: require('./error-handler.js'),
+  formatQuery: require('./format-query.js'),
+  page: require('./page.js'),
+  paginate: require('./paginate.js'),
+  parallel: require('./parallel.js'),
+  redirect: require('./redirect.js'),
+  rest: require('./rest.js'),
+  setLocal: require('./set-local.js'),
+  setMaster: require('./set-master.js'),
+  setTemplate: require('./set-template.js'),
+  shim: require('./shim.js'),
+};
--- middleware/index.js
+++ middleware/index.js
@@ -1,18 +1,6 @@
 'use strict';
 
 module.exports = {
-  bootstrap: require('./bootstrap.js'),
   ensureFound: require('./ensure-found.js'),
-  errorHandler: require('./error-handler.js'),
-  formatQuery: require('./format-query.js'),
-  page: require('./page.js'),
-  paginate: require('./paginate.js'),
-  parallel: require('./parallel.js'),
-  redirect: require('./redirect.js'),
   responder: require('./responder.js'),
-  rest: require('./rest.js'),
-  setLocal: require('./set-local.js'),
-  setMaster: require('./set-master.js'),
-  setTemplate: require('./set-template.js'),
-  shim: require('./shim.js'),
 };
