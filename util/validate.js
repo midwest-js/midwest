b4437f7244131d6e49ccc01f18ca8c5d4354ffd4
Linus Miller <lohfu@lohfu.io>
2017-04-23 18:25:11 +0200

    Fail validation on nully values

--- util/validate.js
+++ util/validate.js
@@ -10,10 +10,12 @@ module.exports = (validation) => function validate(attrs) {
   const errors = [];
 
   _.each(validation, (tests, key) => {
-    if (tests === true) tests = [isNotNil];
-    else if (!Array.isArray(tests)) tests = [tests];
+    if (tests === true) tests = [ isNotNil ];
+    else if (!Array.isArray(tests)) tests = [ tests ];
 
-    if (!tests.every((test) => test(_.get(attrs, key)))) {
+    const value = _.get(attrs, key);
+
+    if (value == null || !tests.every((test) => test(value))) {
       errors.push(key);
     }
   });
