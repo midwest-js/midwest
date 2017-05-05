1b5748b49c41d4434221cc4c91ea064de27318fa
Linus Miller <lohfu@lohfu.io>
2017-05-05 15:33:51 +0200

    Implement location functionality

--- middleware/set-location.js
+++ middleware/set-location.js
@@ -1,22 +1,9 @@
 'use strict';
 
-module.exports = function setLocation(req, res, next) {
-  const protocol = `${req.protocol}:`;
-  const [ pathname, query ] = req.originalUrl.split('?');
-  const host = req.get('host');
-  const [ hostname, port = '' ] = host.split(':');
-  const href = `${protocol}//${host}${req.originalUrl}`;
+const getLocation = require('../util/get-location');
 
-  res.locals.location = Object.assign({
-    host,
-    hostname,
-    protocol,
-    href,
-    pathname,
-    port,
-    url: req.originalUrl,
-    search: query ? `?${query}` : '',
-  });
+module.exports = function setLocation(req, res, next) {
+  res.locals.location = getLocation(req);
 
   next();
 };
---
+++ util/get-location.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = (req) => {
+  const [ pathname, query ] = req.originalUrl.split('?');
+  const host = req.get('host');
+
+  const [ hostname, port = '' ] = host.split(':');
+  const protocol = `${req.protocol}:`;
+  const href = `${protocol}//${host}${req.originalUrl}`;
+
+  return {
+    host,
+    hostname,
+    protocol,
+    href,
+    pathname,
+    port,
+    url: req.originalUrl,
+    search: query ? `?${query}` : '',
+  };
+};
