151dfb77659a3f6561e4c3bbfea6eed2c5dfbb9b
Linus Miller <lohfu@lohfu.io>
2017-05-15 18:10:44 +0200

    Implement ensureFoundStatic

---
+++ middleware/ensure-found-static.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const regex = /\.[a-zA-Z]{1,4}$/;
+
+module.exports = function ensureFoundStatic(req, res, next) {
+  if (regex.test(req.path)) {
+    const err = new Error(`Not found: ${req.method.toUpperCase()} ${req.path}`);
+
+    err.status = 404;
+
+    next(err);
+  } else {
+    next();
+  }
+};
