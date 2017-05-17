092edd7eb3ad00d71215fff415440d02f59415e2
Linus Miller <lohfu@lohfu.io>
2017-05-17 17:46:59 +0200

    More descriptive param name and update comments

--- factories/paginate.js
+++ factories/paginate.js
@@ -9,17 +9,17 @@ const _ = require('lodash');
 
 /* Middleware factory
  *
- * @param {mongoose.Model} Model - Model that will be paginated
- * @param {Number} perPage - Number of results per page
+ * @param {Function} count - Function returning promise for totalCount
+ * @param {Number} defaultLimit - Number of results per page
  *
  * @return A middleware function
  */
-module.exports = (fnc, defaultLimit = 20) => {
+module.exports = (count, defaultLimit = 20) => {
   return function paginate(req, res, next) {
     req.query.offset = parseInt(req.query.offset, 10) || 0;
     req.query.limit = parseInt(req.query.limit, 10) || defaultLimit;
 
-    fnc(_.omit(req.query, 'limit', 'sort', 'offset')).then((totalCount) => {
+    count(_.omit(req.query, 'limit', 'sort', 'offset')).then((totalCount) => {
       res.locals.pagination = {
         offset: req.query.offset,
         limit: req.query.limit,
